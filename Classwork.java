a5105f34a3cac90468189472f1477b92903e5209
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-10-02 23:26:18 +0000

    Completed assignment

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,20 @@
+import java.util.Scanner; //Always have to import Scanner then initialize scanner
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    //Shopping
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What type of item are you buying?");
+    String itemBuying = sc.nextLine(); 
+    System.out.println("How many are you buying?");
+    int amountBought = sc.nextInt();
+    System.out.println("How much does each one weigh?");
+    double weightofProduct = sc.nextDouble();
+    System.out.println(amountBought + itemBuying + " at " + weightofProduct + " pounds each will weigh " + (weightofProduct * amountBought) + " pounds total");
+    //Quote
+    System.out.println("\"That brain of mine is something more than merely mortal; as time will show.\"\nAda Lovelace\nThe first computer programmer");
+    //Rabbit
+    System.out.println("(\\(\\\n(  - -)\n((') (')");
   }
 }
